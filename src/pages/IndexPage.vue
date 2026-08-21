dd3eb63a78a0d3396bd25153c18da30b1f261cde
Yotsakorn <kmegumi354@gmail.com>
2026-08-21 15:27:37 +0700

    V.6-Button with Japanese language-XXX

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -10,8 +10,8 @@
       <q-input v-model="age" filled type="number" label="Your age *" />
       <q-toggle v-model="accept" label="I accept the license and terms" />
       <div>
-        <q-btn label="SUBMIT" type="submit" color="primary" />
-        <q-btn label="RESET" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="SUBMIT (提出する)" type="submit" color="primary" />
+        <q-btn label="RESET (リセット)" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
   </q-page>
