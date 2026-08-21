61cec4318b848e7431184a93ac67bfb209955788
Yotsakorn <kmegumi354@gmail.com>
2026-08-21 15:22:11 +0700

    v.5-Accept with Chinese language-371

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,8 +6,8 @@
       class="q-gutter-md"
       style="max-width: 600px"
     >
-      <q-input v-model="name" filled label="Your name *" hint="Name and surname" />
-      <q-input v-model="age" filled type="number" label="Your age *" />
+    <q-input v-model="name" filled label="Your name (ชื่อ-สกุล) *" hint="Name and surname" />
+      <q-input v-model="age" filled type="number" label="Your age (อายุ) *" />
       <q-toggle v-model="accept" label="I accept the license and terms (我接受许可和条款语言)" />
 
       <div>
